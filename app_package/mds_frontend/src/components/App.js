29ccfb862d1ec9424acd43879a047337f0730251
shntd2 <pmpm68036@gmail.com>
2024-06-26 22:14:27 +0300

    Updated React part of the frontend

--- app_package/mds_frontend/src/components/App.js
+++ app_package/mds_frontend/src/components/App.js
@@ -1,1 +1,13 @@
 // root React component
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Test message</h1>
+        </div>
+    );
+};
+
+export default App;
